1368cd8067d6ab5463d44ed3b2d1ce32401163af
dsantosr <dsantrodrigues42@gmail.com>
2024-04-27 00:32:23 -0300

    Fixing font-size responsivity

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-//Função para interação da logo com o clique do mouse !!NEED TO FIX
-document.addEventListener('DOMContentLoaded', function() {
-  const logoImg = document.querySelector('.header__logo'); // Seleciona o elemento img
-  const logoPath = logoImg.querySelector('path'); // Seleciona o elemento path dentro do img
-  const highlightColor = getComputedStyle(document.documentElement).getPropertyValue('--highlight-color').trim();
-
-  // Adiciona evento de clique ao elemento img
-  logoImg.addEventListener('mousedown', function() {
-      logoPath.style.fill = highlightColor; // Define a cor como a variável --highlight-color
-  });
-
-  // Adiciona evento de clique ao elemento img para restaurar a cor original
-  logoImg.addEventListener('mouseup', function() {
-      logoPath.style.fill = ''; // Volta para a cor original
-  });
-});
-
 // Função para a estilização da barra de scroll do header
 document.addEventListener('scroll', function() {
   //Calcula a distância máxima de rolagem
